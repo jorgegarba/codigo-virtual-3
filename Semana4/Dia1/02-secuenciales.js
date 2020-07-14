55128f28ea4ac9b1e4aa96d66c5e7e037ca567d6
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-07-13 22:09:26 -0500

    Subiendo ejercicios resueltos

---
+++ Semana4/Dia1/02-secuenciales.js
@@ -0,0 +1,25 @@
+// Ejemplo 2.2
+// declaración de variables
+let nota1 = 13;
+let nota2 = 15;
+let nota3 = 20;
+let nota4 = 10;
+
+let suma = 0;
+let rpta = 0;
+// resolver el problema
+suma = nota1 + nota2 + nota3 + nota4;
+rpta = suma / 4;
+// mostrar el resultado
+console.log(`Nota Final: ${rpta}`);
+
+// Ejemplo 2.9
+
+// Declarar variables
+let horasTrabajadas = 50;
+let horasExtras = 10;
+let precioPorHora = 2;
+
+console.log(`Total a pagar ${(horasTrabajadas*precioPorHora) + (horasExtras*precioPorHora*2)}`);
+
+
---
+++ Semana4/Dia1/03-secuenciales2.js
@@ -0,0 +1,10 @@
+// 2.7 propuesto
+
+let costoBoleto = 0;
+
+let recorrer = +prompt("Ingrese los kilometros a recorrer");
+let costoKilometro = +prompt("Ingrese el costo por Kilometro");
+
+costoBoleto = recorrer * costoKilometro;
+
+console.log(`Costo por boleto => ${costoBoleto} soles.`);
