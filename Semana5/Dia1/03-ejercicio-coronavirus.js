b6c87dc661e30283caae6c9eccc325df37facd95
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-07-20 20:05:30 -0500

    Subiendo planteamiento de ejercicios con objetos parte 2

---
+++ Semana5/Dia1/03-ejercicio-coronavirus.js
@@ -0,0 +1,35 @@
+//1
+/**
+ * Función que retorna el número de casos de personas 
+ * infectadas en el total en el mundo
+ */
+const getTotalDeCasosEnElMundo = ()=>{
+
+}
+
+
+//2
+/**
+ * Función que retorna un objeto completo de cifras
+ * de un pais dado su nombre, en caso no encuentre el pais
+ * RETORNAR NULL
+ * @param {*} pais 
+ */
+const getCifrasByNamePais = (pais)=>{
+
+}
+
+
+//3
+/**
+ * Función que retorna el porcentaje de casos de personas
+ * recuperadas frente al total de casos en un pais
+ * 
+ * Para ello se debe proporcionar el nombre del país.
+ * Para buscar un país, se debe hacer uso de la función del
+ * ejercicio 2 (OBLIGATORIO)
+ * @param {*} pais 
+ */
+const getPorcentajeDeRecuperadosByNamePais = (pais)=>{
+
+}
