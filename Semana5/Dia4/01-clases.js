7b1fd196abc5ac0e5df71fcb9da2c94e6a37507c
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-07-23 20:22:15 -0500

    Clases y objetos

---
+++ Semana5/Dia4/01-clases.js
@@ -0,0 +1,83 @@
+/**
+ * Los nombres de las clases deben iniciar con una Mayúscula
+ */
+class Restaurant {
+  aforo;
+  nroMesas;
+  direccion;
+  categoria;
+  telefonos;
+  nombre;
+  delivery;
+  /**
+   * Para la 'tipificacion':
+   * Será tipo "A" si el aforo es mayor a 500 personas
+   * Será tipo "B" si es aforo es mayor a 300 personas
+   * Será tipo "C" si el aforo es menor igual a 300 personas
+   */
+  tipificacion;
+
+  /**
+   * Funcion que se ejecuta automaticamente al momento de crear un objeto
+   */
+  constructor(
+    _aforo = 0,
+    _nroMesas = 0,
+    _direccion = "Sin Dirección",
+    _categoria = "Sin Categoria",
+    _telefonos = [],
+    _nombre = "Sin Nombre",
+    _delivery = false
+  ) {
+    // this : acceder al scope interno de la clase.
+    // this: se usa para acceder a los atributos y métodos de la clase.
+    this.aforo = _aforo;
+    this.nroMesas = _nroMesas;
+    this.direccion = _direccion;
+    this.categoria = _categoria;
+    this.telefonos = _telefonos;
+    this.nombre = _nombre;
+    this.delivery = _delivery;
+
+    if (this.aforo > 500) {
+      this.tipificacion = "A";
+    } else if (this.aforo > 300) {
+      this.tipificacion = "B";
+    } else {
+      this.tipificacion = "C";
+    }
+  }
+
+  imprimirTelefonos() {
+    console.log(`Teléfonos del restaurant: ${this.nombre}`);
+    for (let i = 0; i < this.telefonos.length; i++) {
+      console.log(`📞 ${this.telefonos[i]}`);
+    }
+  }
+
+  imprimirTelefonos2() {
+    for (const tel of this.telefonos) {
+      console.log(`Telefono: ${tel}`);
+    }
+  }
+}
+
+// Instanciando una clase : Creando un objeto a partir de una clase
+let objTanta = new Restaurant(
+  800,
+  200,
+  "Vallecito",
+  "Comida Criolla",
+  ["974204853", "974854512"],
+  "Tanta",
+  true
+);
+let objAstrid = new Restaurant();
+// Modificando los atributos de un objeto
+// objAstrid.aforo = 1000;
+
+console.log(objTanta);
+console.log(objAstrid);
+
+objTanta.imprimirTelefonos();
+objTanta.imprimirTelefonos2();
