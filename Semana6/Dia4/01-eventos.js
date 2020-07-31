4c85dd38234fff753fca0dbf92f7f47b25efa8d1
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-07-30 22:09:20 -0500

    eventos parte 2

--- Semana6/Dia4/01-eventos.js
+++ Semana6/Dia4/01-eventos.js
@@ -3,6 +3,8 @@ let btnDark = document.getElementById("btnDark");
 let btnToggle = document.getElementById("btnToggle");
 let linkDark = document.getElementById("linkDark");
 let cuadrado = document.getElementById("cuadrado");
+let cuadrado2 = document.getElementById("cuadrado2");
+let cuadrado3 = document.getElementById("cuadrado3");
 
 btnLight.onclick = () => {
   console.log("click en light");
@@ -29,12 +31,48 @@ btnToggle.onclick = (e) => {
 };
 
 cuadrado.onclick = (e) => {
-	console.log(e);
-	console.log(`Tipo de evento => ${e.type}`);
-	console.log("Elemento objetivo del evento:");
-	console.log(e.target);
-	console.log(`Valores de X e Y con respecto al VIEWPORT`);
-	console.log(`X => ${e.clientX} - Y => ${e.clientY}`);
-	console.log(`Valores de X e Y con respecto al MISMO ELEMENTO`);
-	console.log(`X => ${e.offsetX} - Y => ${e.offsetY}`);
+  console.log(e);
+  console.log(`Tipo de evento => ${e.type}`);
+  console.log("Elemento objetivo del evento:");
+  console.log(e.target);
+  console.log(`Valores de X e Y con respecto al VIEWPORT`);
+  console.log(`X => ${e.clientX} - Y => ${e.clientY}`);
+  console.log(`Valores de X e Y con respecto al MISMO ELEMENTO`);
+  console.log(`X => ${e.offsetX} - Y => ${e.offsetY}`);
+};
+
+/**
+ * elemento.onmousemove
+ * evento que se desencadena cuando el mouse se mueve por encima del elemento
+ */
+
+cuadrado2.onmousemove = (e) => {
+  let x = e.offsetX;
+  let y = e.offsetY;
+  if (e.ctrlKey) {
+    let bolita = document.createElement("div");
+    bolita.style.left = x + "px";
+    bolita.style.top = y + "px";
+    bolita.style.display = "block";
+    bolita.classList.add("bolita");
+    cuadrado2.appendChild(bolita);
+  }
+};
+
+/**
+ * elemento.onmouseover
+ * evento que se ejecuta cuando el mouse pasa por encima del elemento
+ * A diferencia de mousemove, el evento sólo se dispara UNA VEZ, cuando el mouse
+ * ingresa al elemento.
+ * SIMILAR A HOVER (CSS)
+ */
+
+cuadrado3.onmouseover = (e) => {
+  console.log(e);
+  cuadrado3.classList.add("sombra");
+};
+
+cuadrado3.onmouseleave = (e) => {
+  console.log(e);
+  cuadrado3.classList.remove("sombra");
 };
---
+++ Semana6/Dia4/02-eventos-formularios.js
@@ -0,0 +1,18 @@
+/**
+ * elemento.onload
+ * función que se dispara cuando se han cargado todos los elementos hijos
+ * de un elemento
+ */
+window.onload = () => {
+
+    
+
+  let inputPassword = document.getElementById("inputPassword");
+  let formulario = document.getElementById("formulario");
+  let inputEmail = document.getElementById("inputEmail");
+  let enlace = document.getElementById("enlace");
+  console.log(formulario);
+
+
+
+};
