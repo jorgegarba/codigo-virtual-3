591af33523ea8f73131e1f3a0992caa657c46880
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-07-31 20:04:22 -0500

    Validando formularios

--- Semana6/Dia4/02-eventos-formularios.js
+++ Semana6/Dia4/02-eventos-formularios.js
@@ -4,15 +4,52 @@
  * de un elemento
  */
 window.onload = () => {
-
-    
-
-  let inputPassword = document.getElementById("inputPassword");
   let formulario = document.getElementById("formulario");
+  let inputPassword = document.getElementById("inputPassword");
   let inputEmail = document.getElementById("inputEmail");
   let enlace = document.getElementById("enlace");
-  console.log(formulario);
+  let helperEmail = document.getElementById("helperEmail");
 
+  const validarEmail = (email) => {
+    let resultados = usuarios.filter((objUsuario) => {
+      if (objUsuario.correo === email) {
+        return objUsuario.correo;
+      }
+    });
 
+    if (resultados.length > 0) {
+      helperEmail.innerText = "ERRORSH! el email ya está siendo usado";
+      helperEmail.classList.remove("text-success");
+      helperEmail.classList.add("text-danger");
+      inputEmail.setAttribute("class","input-danger");
+    } else {
+      helperEmail.innerText = "BIEN! el email está disponible";
+      helperEmail.classList.remove("text-danger");
+      helperEmail.classList.add("text-success");
+      inputEmail.setAttribute("class","input-success");
+    }
+  };
 
+  // const validarEmail = (email) => {
+  //   let resultados = usuarios.filter(
+  //     (objUsuario) => objUsuario.correo === email
+  //   );
+
+  //   helperEmail.innerText =
+  //     resultados.length > 0
+  //       ? "ERRORSH! el email ya está siendo usado"
+  //       : "BIEN! el email está disponible";
+  // };
+
+  /**
+   * Funcion que se ejecuta cuando una tecla es presionada y soltada
+   * sobre un elemento objetivo
+   * @param {*} e
+   */
+  inputEmail.onkeyup = (e) => {
+    // console.log(e);
+    // console.log(e.key);
+    // console.log("presionando una tecla");
+    validarEmail(inputEmail.value.trim());
+  };
 };
---
+++ Semana6/Dia4/data-usuarios.js
@@ -0,0 +1,402 @@
+let usuarios = [
+  {
+    correo: "tortor.Integer.aliquam@placerateget.ca",
+    nombre: "Fuller Whitney",
+  },
+  {
+    correo: "Ut.tincidunt@scelerisque.com",
+    nombre: "Alisa Rocha",
+  },
+  {
+    correo: "erat.Etiam.vestibulum@MorbimetusVivamus.co.uk",
+    nombre: "Jessamine Garrison",
+  },
+  {
+    correo: "pede@quam.co.uk",
+    nombre: "Declan Webster",
+  },
+  {
+    correo: "magna@egestasadui.co.uk",
+    nombre: "Raja Macias",
+  },
+  {
+    correo: "ultrices@elementum.org",
+    nombre: "Francis Robbins",
+  },
+  {
+    correo: "at.augue@elit.com",
+    nombre: "Sheila Graham",
+  },
+  {
+    correo: "Duis@Nullasempertellus.edu",
+    nombre: "Hashim Hall",
+  },
+  {
+    correo: "luctus.aliquet.odio@id.edu",
+    nombre: "Lara Hutchinson",
+  },
+  {
+    correo: "lobortis.augue@Nunc.edu",
+    nombre: "William Mckee",
+  },
+  {
+    correo: "bibendum@amifringilla.com",
+    nombre: "Gregory Mckee",
+  },
+  {
+    correo: "semper.auctor@eu.com",
+    nombre: "Calista Le",
+  },
+  {
+    correo: "egestas.Sed@scelerisque.co.uk",
+    nombre: "Emerson Mendez",
+  },
+  {
+    correo: "Donec.est@acmieleifend.co.uk",
+    nombre: "Nissim Salas",
+  },
+  {
+    correo: "eu.metus@Inscelerisque.ca",
+    nombre: "Wang Jensen",
+  },
+  {
+    correo: "euismod@rutrumFuscedolor.net",
+    nombre: "Eugenia Hamilton",
+  },
+  {
+    correo: "quis.turpis@blanditviverraDonec.com",
+    nombre: "Tarik Brewer",
+  },
+  {
+    correo: "eget.magna.Suspendisse@rhoncusProin.net",
+    nombre: "Tanner Holder",
+  },
+  {
+    correo: "a.purus@malesuadafames.com",
+    nombre: "Lael Ratliff",
+  },
+  {
+    correo: "sodales.nisi@fermentum.edu",
+    nombre: "Amity Robertson",
+  },
+  {
+    correo: "at@justoeu.edu",
+    nombre: "Neil Hutchinson",
+  },
+  {
+    correo: "nibh.Aliquam@egetmagna.org",
+    nombre: "Sebastian Valencia",
+  },
+  {
+    correo: "ligula@noncursus.ca",
+    nombre: "Jamal Valdez",
+  },
+  {
+    correo: "ipsum@Aeneanegetmagna.co.uk",
+    nombre: "Boris Bentley",
+  },
+  {
+    correo: "risus@Donecporttitortellus.edu",
+    nombre: "Destiny Smith",
+  },
+  {
+    correo: "quis.diam@vulputateduinec.org",
+    nombre: "Ralph Ashley",
+  },
+  {
+    correo: "aptent@felisadipiscingfringilla.com",
+    nombre: "Lacota Browning",
+  },
+  {
+    correo: "amet@semper.org",
+    nombre: "Hayfa Brooks",
+  },
+  {
+    correo: "consectetuer@gravidamolestie.ca",
+    nombre: "Davis Pace",
+  },
+  {
+    correo: "eu@sodalespurus.org",
+    nombre: "Nomlanga King",
+  },
+  {
+    correo: "lectus.a.sollicitudin@mattisornarelectus.net",
+    nombre: "Karina Evans",
+  },
+  {
+    correo: "aliquet@lacinia.edu",
+    nombre: "Sloane Floyd",
+  },
+  {
+    correo: "mi.tempor.lorem@mattisIntegereu.co.uk",
+    nombre: "Jada Hammond",
+  },
+  {
+    correo: "Donec.tempus@blanditcongue.ca",
+    nombre: "Jasmine Hall",
+  },
+  {
+    correo: "nec.enim.Nunc@velfaucibus.ca",
+    nombre: "Kylan Delgado",
+  },
+  {
+    correo: "ridiculus@Loremipsumdolor.org",
+    nombre: "Kerry Dodson",
+  },
+  {
+    correo: "lacus.varius@duiFuscealiquam.com",
+    nombre: "Christine Reed",
+  },
+  {
+    correo: "nec.diam@Nunc.com",
+    nombre: "Herman Dennis",
+  },
+  {
+    correo: "risus.Quisque@iaculisaliquet.com",
+    nombre: "Solomon Lynn",
+  },
+  {
+    correo: "sed.consequat.auctor@Maurisquisturpis.ca",
+    nombre: "Chanda Hendricks",
+  },
+  {
+    correo: "mus.Proin@SednequeSed.edu",
+    nombre: "Leroy Graham",
+  },
+  {
+    correo: "vel.venenatis.vel@leoCras.org",
+    nombre: "Zoe Harper",
+  },
+  {
+    correo: "consectetuer@sociis.ca",
+    nombre: "Montana Kerr",
+  },
+  {
+    correo: "Quisque.ac.libero@Aliquamrutrumlorem.edu",
+    nombre: "Ferris Lowe",
+  },
+  {
+    correo: "et.netus.et@mi.ca",
+    nombre: "Ifeoma Ayers",
+  },
+  {
+    correo: "Cum.sociis.natoque@blanditat.ca",
+    nombre: "Dora Short",
+  },
+  {
+    correo: "Curabitur.egestas@orci.org",
+    nombre: "Jesse Anthony",
+  },
+  {
+    correo: "ac.ipsum@congueelit.co.uk",
+    nombre: "Raphael Smith",
+  },
+  {
+    correo: "a@Loremipsumdolor.com",
+    nombre: "Lois Snider",
+  },
+  {
+    correo: "Nullam.velit.dui@arcu.edu",
+    nombre: "Slade Chen",
+  },
+  {
+    correo: "Nullam.ut.nisi@urna.com",
+    nombre: "Karen Ayala",
+  },
+  {
+    correo: "eu.metus@diamDuismi.ca",
+    nombre: "Lyle Gaines",
+  },
+  {
+    correo: "aliquet.Proin.velit@lobortisultrices.co.uk",
+    nombre: "Emerson Mccall",
+  },
+  {
+    correo: "ante.dictum@ultriciesadipiscing.com",
+    nombre: "Yen Key",
+  },
+  {
+    correo: "Curabitur.ut.odio@sociisnatoquepenatibus.co.uk",
+    nombre: "Ali Mcleod",
+  },
+  {
+    correo: "rutrum.non@Sed.net",
+    nombre: "India Mills",
+  },
+  {
+    correo: "velit.dui.semper@Pellentesquehabitantmorbi.net",
+    nombre: "Samuel Strickland",
+  },
+  {
+    correo: "auctor@justo.net",
+    nombre: "Jessica Koch",
+  },
+  {
+    correo: "euismod.et.commodo@seddictum.ca",
+    nombre: "Aiko Emerson",
+  },
+  {
+    correo: "semper@cursus.co.uk",
+    nombre: "Harrison Holloway",
+  },
+  {
+    correo: "accumsan.neque.et@atliberoMorbi.org",
+    nombre: "Danielle Patterson",
+  },
+  {
+    correo: "urna.convallis.erat@Fuscealiquamenim.net",
+    nombre: "Wesley Mercer",
+  },
+  {
+    correo: "dictum@eueuismodac.org",
+    nombre: "Faith Rollins",
+  },
+  {
+    correo: "dolor.Donec.fringilla@Cum.edu",
+    nombre: "Adena Oneil",
+  },
+  {
+    correo: "penatibus@augueeutempor.edu",
+    nombre: "Darrel Washington",
+  },
+  {
+    correo: "Quisque.fringilla.euismod@velitQuisque.ca",
+    nombre: "Vera Stewart",
+  },
+  {
+    correo: "elit.a@tellusfaucibusleo.co.uk",
+    nombre: "Ahmed Montoya",
+  },
+  {
+    correo: "tempor@justositamet.co.uk",
+    nombre: "Heather Atkinson",
+  },
+  {
+    correo: "eu.enim.Etiam@nectempus.net",
+    nombre: "Kelly Powell",
+  },
+  {
+    correo: "lorem.ut.aliquam@pretium.edu",
+    nombre: "Jolie Burnett",
+  },
+  {
+    correo: "litora.torquent.per@feugiat.edu",
+    nombre: "Honorato Mcclain",
+  },
+  {
+    correo: "non.quam.Pellentesque@Donecdignissim.co.uk",
+    nombre: "Austin David",
+  },
+  {
+    correo: "Praesent@Inmi.org",
+    nombre: "Katell Davidson",
+  },
+  {
+    correo: "gravida.Aliquam@acfacilisis.co.uk",
+    nombre: "Slade Cooke",
+  },
+  {
+    correo: "parturient.montes@milacinia.ca",
+    nombre: "Nicholas Charles",
+  },
+  {
+    correo: "ultricies@habitantmorbi.ca",
+    nombre: "Emerson Cantrell",
+  },
+  {
+    correo: "sit@scelerisquesed.edu",
+    nombre: "Wallace Vang",
+  },
+  {
+    correo: "Cras.interdum@adipiscingelit.co.uk",
+    nombre: "Neil Hyde",
+  },
+  {
+    correo: "amet@blanditenim.edu",
+    nombre: "Raphael Bentley",
+  },
+  {
+    correo: "congue@orciadipiscing.edu",
+    nombre: "Mira Hahn",
+  },
+  {
+    correo: "imperdiet.erat.nonummy@Utnecurna.org",
+    nombre: "Robert Fowler",
+  },
+  {
+    correo: "Pellentesque.habitant.morbi@turpisnecmauris.com",
+    nombre: "Dominique Sanders",
+  },
+  {
+    correo: "eros.Nam.consequat@metusAliquamerat.org",
+    nombre: "Kasimir Horn",
+  },
+  {
+    correo: "aliquam.iaculis.lacus@semegestasblandit.org",
+    nombre: "Halee Witt",
+  },
+  {
+    correo: "dictum.placerat.augue@Morbisitamet.org",
+    nombre: "Winter Hopper",
+  },
+  {
+    correo: "sem.egestas.blandit@metus.ca",
+    nombre: "Naomi Sims",
+  },
+  {
+    correo: "et.rutrum.non@Aeneanegestashendrerit.com",
+    nombre: "Freya Fowler",
+  },
+  {
+    correo: "risus@et.co.uk",
+    nombre: "Martina Mcintyre",
+  },
+  {
+    correo: "a.arcu.Sed@quisturpisvitae.edu",
+    nombre: "Rigel Wilder",
+  },
+  {
+    correo: "Sed.eu.nibh@Nullam.co.uk",
+    nombre: "Abraham Stephens",
+  },
+  {
+    correo: "nibh.Donec.est@Fusce.co.uk",
+    nombre: "Whoopi Cline",
+  },
+  {
+    correo: "parturient@infelisNulla.org",
+    nombre: "Desiree Edwards",
+  },
+  {
+    correo: "dolor.dolor@IntegermollisInteger.net",
+    nombre: "Coby Vang",
+  },
+  {
+    correo: "a@Curabitur.co.uk",
+    nombre: "Kiayada Wolfe",
+  },
+  {
+    correo: "Sed.nulla.ante@arcuMorbi.com",
+    nombre: "Chancellor Olsen",
+  },
+  {
+    correo: "dolor.tempus@montesnasceturridiculus.ca",
+    nombre: "Mannix Lawrence",
+  },
+  {
+    correo: "nec.leo.Morbi@nequetellus.org",
+    nombre: "Lewis Small",
+  },
+  {
+    correo: "at@Donecconsectetuermauris.org",
+    nombre: "Basil Wilcox",
+  },
+  {
+    correo: "Vivamus.nibh.dolor@Integeridmagna.org",
+    nombre: "Gisela Trujillo",
+  },
+  {
+    correo: "Morbi@laoreetlibero.org",
+    nombre: "Mufutau Gibson",
+  },
+];
