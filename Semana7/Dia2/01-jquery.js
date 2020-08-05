0a125923aa49e4b5dc8eab5549470f13724fd913
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-08-04 20:16:15 -0500

    instalando jquery

---
+++ Semana7/Dia2/01-jquery.js
@@ -0,0 +1,8 @@
+// Capturar elementos con JQuery
+// $("#btnCambiarParrafo")
+
+// eventos en JQuery
+$("#btnCambiarParrafo").click(() => {
+  //modificar el texto (innerText) de un elemento
+  $("#parrafo1").text("Ya es hora del break");
+});
