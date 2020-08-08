71f69dac83a14785ed4e5cfb04aa55c499d8b7d0
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-08-07 20:12:56 -0500

    Libreria Flickity

--- Semana7/Dia5/01-waves/js/index.js
+++ Semana7/Dia5/01-waves/js/index.js
@@ -1,2 +1,11 @@
-Waves.attach(".btn");
-Waves.init();
+// (() => {
+//   Waves.attach(".btn");
+//   Waves.init();
+// })();
+
+const inicializarTodo = () => {
+  Waves.attach(".btn");
+  Waves.init();
+};
+
+inicializarTodo();
---
+++ Semana7/Dia5/02-flickity/js/index.js
@@ -0,0 +1,2 @@
+let carousel = document.querySelector(".carousel");
+let fli = new Flickity(carousel, { groupCells: true });
