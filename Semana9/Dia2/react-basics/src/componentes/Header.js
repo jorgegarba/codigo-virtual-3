2b7e1fb24cc317e968e95168c8198214676e6ed4
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-08-18 20:12:55 -0500

    Entendiendo useState

---
+++ Semana9/Dia2/react-basics/src/componentes/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Link 1</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+          <li>Link 4</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
