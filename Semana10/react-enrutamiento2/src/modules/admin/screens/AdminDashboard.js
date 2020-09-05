a965adc0c9c668ad8fbbcb87a15716c295264afb
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-09-04 22:10:04 -0500

    Preparando consumo de sockets mediante el dashboard

--- Semana10/react-enrutamiento2/src/modules/admin/screens/AdminDashboard.js
+++ Semana10/react-enrutamiento2/src/modules/admin/screens/AdminDashboard.js
@@ -1,11 +1,71 @@
-import React from 'react'
-
+import React from "react";
+import { useEffect } from "react";
+import io from "socket.io-client";
+import { URL_BACKEND_HEROKU } from "../../../enviroment/enviroment";
+import { useState } from "react";
 const AdminDashboard = () => {
-  return (
-    <div>
-      Página de Admin Dashoboard
-    </div>
-  )
-}
+  // ir haciendo logica para consuimir sockets del bacjend
+  const [reporte, setReporte] = useState({
+    pedidos: "cargando...",
+    mesas: "cargando...",
+    categorias: "cargando...",
+  });
+  useEffect(() => {
+    const socket = io(URL_BACKEND_HEROKU);
+    socket.on("reporte-dashboard", (dataString) => {
+      const dataJSON = JSON.parse(dataString);
+      setReporte(dataJSON);
+      /**
+       * {
+       *  pedidos: 5,
+       *  mesas: 7,
+       *  categorias: 9
+       * }
+       */
+    });
+  }, []);
 
-export default AdminDashboard
+  return (
+    <>
+      <div className="row mt-5">
+        <div className="col">
+          <h1 className="display-4 text-center">DASHBOARD</h1>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col-md-4">
+          <div className="card shadow">
+            <div className="card-header">
+              <h5 className="card-title text-center">Pedidos</h5>
+            </div>
+            <div className="card-body">
+              <h5 className="display-4 text-center">{reporte.pedidos}</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card shadow">
+            <div className="card-header">
+              <h5 className="card-title text-center">Mesas</h5>
+            </div>
+            <div className="card-body">
+              <h5 className="display-4 text-center">{reporte.mesas}</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card shadow">
+            <div className="card-header">
+              <h5 className="card-title text-center">Categorias</h5>
+            </div>
+            <div className="card-body">
+              <h5 className="display-4 text-center">{reporte.categorias}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminDashboard;
